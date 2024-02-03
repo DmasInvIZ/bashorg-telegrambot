3f755be06d90eb5c42c5eabba7edcbe154671e79
Dmitry <inviz1806@gmail.com>
2024-02-03 11:41:30 +0300

    готовая версия

--- bashorg-bot.py
+++ bashorg-bot.py
@@ -11,6 +11,10 @@ from telebot import types
 import requests
 from bs4 import BeautifulSoup
 from token_file import token
+import logging
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
 
 bot = telebot.TeleBot(token)
 url = "https://xn--80abh7bk0c.xn--p1ai/random"
@@ -29,28 +33,34 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['random'])
 def send_quote(message):
-
     """Отправка рандомной цитаты"""
-    print('Запрос от {0.first_name}'.format(message.from_user))  #########
-    response = requests.get(url)                                            # получаем страницу от сервера
-    soup = BeautifulSoup(response.text, 'lxml')                             # создаем объект html страницы
+    try:
+        print('Запрос от {0.first_name}'.format(message.from_user))                  #########
+        response = requests.get(url)                                                 # получаем страницу от сервера
+        soup = BeautifulSoup(response.text, 'lxml')                                  # создаем объект html страницы
+        date = soup.find('div', class_='quote__header_date').text.strip()[0:10]      # получаем дату
+        quote_num = soup.find('a', class_='quote__header_permalink').text            # получаем номер цитаты
+        quote_num_link = soup.find('a', class_='quote__header_permalink').get('href')  # получаем ссылку на цитату
+        dirty_quote = str(soup.find('div', class_='quote__frame').find('div', class_='quote__body'))  # строка \
+                                                                 # c цитатой со спецсимволами и со склеенными строками
+        quote = dirty_quote.replace("<br/>", "\n").replace('<div class="quote__body">', "").replace("</div>", "")\
+            .replace('&lt;', '-').replace('&gt;', '-').strip()                       # готовая строка с цитатой
+        link = hlink(quote_num, f'https://xn--80abh7bk0c.xn--p1ai{quote_num_link}')  # формируем ссылку для телеграмм
 
-    date = soup.find('div', class_='quote__header_date').text.strip()[0:10]  # получаем дату
-    quote_num = soup.find('a', class_='quote__header_permalink').text  # получаем номер цитаты
-    print(quote_num)
-    quote_num_link = soup.find('a', class_='quote__header_permalink').get('href')
-    print(quote_num_link)
-    dirty_quote = str(soup.find('div', class_='quote__frame').find('div', class_='quote__body'))  # строка с цитатой со спец символами и со склееными строками
-    quote = dirty_quote.replace("<br/>", "\n").replace('<div class="quote__body">', "").replace("</div>", "")\
-        .replace('&lt;', '<').replace('&gt;', '>').strip()  # готовая строка с цитатой
+        answer = str(f'{link} - Добавлено {date}\n{quote}')                                    # формируем ответ для пользователя
+        # print(answer)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/random")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, answer, reply_markup=markup, parse_mode='HTML')
+        logger.info('Запрос от {0.first_name}'.format(message.from_user))
 
-    #link = hlink(f"{quote_num}", "https://xn--80abh7bk0c.xn--p1ai{quote_num_link}")
-    #print(link)
-    answer = f'{quote_num} - {date}' + "\n" + quote
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("/random")
-    markup.add(btn1)
-    bot.send_message(message.chat.id, answer, reply_markup=markup, parse_mode="HTML")
+    except Exception as error:
+        print(error)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("/random")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Что-то не так...", reply_markup=markup, parse_mode='HTML')
 
 
 @bot.message_handler(content_types=['text'])
