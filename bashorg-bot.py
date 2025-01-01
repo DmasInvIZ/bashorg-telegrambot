837e841554fd8ae510ada496b652c889cb68642d
Dmitry <inviz1806@gmail.com>
2025-01-01 23:09:33 +0300

    работает, обошлись без бд, все цитаты сохраняются в список внутри программы

--- bashorg-bot.py
+++ bashorg-bot.py
@@ -15,37 +15,6 @@ import logging
 from threading import Timer
 import random
 import datetime
-import sqlite3
-import os
-from os import path
-
-
-def create_database():
-    """Cоздаем БД и ее таблицы, если их нет"""
-    os.chdir(r"D:\Prog\Python\Projects\bashorg-telegrambot")
-    if not path.exists("sqdb.db"):
-        try:
-            print("Создаем БД")
-            connection = sqlite3.connect("sqdb.db")
-            cursor = connection.cursor()
-
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS Quotes (
-                id INTEGER PRIMARY KEY,
-                quote TEXT
-                )
-                ''')
-
-            connection.commit()
-
-        except Exception as error:
-            print(error)
-
-        else:
-            connection.close()
-
-    else:
-        print("БД существует")
 
 
 keywords = "шутк", ",бугага", "смешн", "смех", "шути", "юмор", "прикол", "смех", "сарказм", "ирония", "гэг", \
@@ -62,20 +31,25 @@ url = "https://xn--80abh7bk0c.xn--p1ai/random"
 timer = None
 chat_id = None
 
+quotes_array = []
+
 
 def timer_for_joke():
     global timer
     if timer:
         timer.cancel()
-    if "21:00:00" < datetime.datetime.now().strftime("%H:%M:%S") < "9:00:00":
-        print("Сейчас не время")
-        next_joke_time = random.randint(60*60*3, 60*60*6)
-        print(f"Таймер сработает через {next_joke_time} секунд")
+    if "21:00:00" < datetime.datetime.now().strftime("%H:%M:%S") < "9:00:00":       # время в которое шуток не будет
+        # print("Сейчас не время")
+        logging.info("Сейчас не время")
+        next_joke_time = random.randint(60*60*3, 60*60*6)        # время до следующей шутки в секундах
+        # print(f"Таймер сработает через {next_joke_time/60} минут")
+        logging.info(f"Таймер сработает через {round(next_joke_time/60)} минут")
         timer = Timer(next_joke_time, timer_for_joke)
         timer.start()
     else:
         next_joke_time = random.randint(60*60*1, 60*60*5)
-        print(f"Следующая цитата через {next_joke_time} секунд")
+        # print(f"Следующая цитата через {round(next_joke_time/60)} минут")
+        logging.info(f"Следующая цитата через {round(next_joke_time/60)} минут")
         timer = Timer(next_joke_time, joking)  # изменить время на рандомное
         timer.start()  # настроить таймер чтобы не срабатывал ночью
 
@@ -108,79 +82,62 @@ def send_quote(message):
         logger.info('Запрос от {0.first_name}'.format(message.from_user))
 
     except Exception as error:
-        print(error)
+        # print(error)
+        logging.info(error)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("/random")
         markup.add(btn1)
-        bot.send_message(message.chat.id, "Что-то не так...", reply_markup=markup, parse_mode='HTML')
+        bot.reply_to(message.chat.id, "Что-то не так...", reply_markup=markup, parse_mode='HTML')
 
 
 def load_quotes():
-    response = requests.get(url, verify=False)  # получаем страницу от сервера
-    soup = BeautifulSoup(response.text, 'lxml')  # создаем объект html страницы
+    """Загружает цитаты в массив"""
+    response = requests.get(url, verify=False)                                          # получаем страницу от сервера
+    soup = BeautifulSoup(response.text, 'lxml')                                         # создаем объект html страницы
     articles = soup.find('section', class_='quotes')
     article = articles.find_all('article', class_='quote')
     if article:
-        connection = sqlite3.connect("sqdb.db")
-        cursor = connection.cursor()
         try:
             for i in article:
                 quote_date = i.find('header', class_='quote__header').find('div',
-                                                                                 class_='quote__header_date').text.strip()[0:10]
+                    class_='quote__header_date').text.strip()[0:10]                         # получаем дату
                 quote_number = i.find('header', class_='quote__header').find('a',
-                                                                                   class_='quote__header_permalink').text
+                            class_='quote__header_permalink').text                          # получаем номер цитаты
                 quote_link = i.find('header', class_='quote__header').find('a',
-                                                                                 class_='quote__header_permalink').get('href')
+                            class_='quote__header_permalink').get('href')                   # получаем ссылку на цитату
                 quote_text = str(i.find('div', class_='quote__body')) \
                     .replace('<br/>', '\n') \
                     .replace('</div>', '') \
                     .replace('<div class="quote__body">', '') \
                     .replace('&lt;', '  ') \
-                    .replace('&gt;', '  -').strip()  # готовая строка с цитатой
-                link = hlink(quote_number, f'https://xn--80abh7bk0c.xn--p1ai{quote_link}')
-                quote = str(f'{link} - Добавлено {quote_date}\n{quote_text}')
-                print(quote)
-                cursor.execute("INSERT INTO Quotes (quote) VALUES (?)", (quote,))
+                    .replace('&gt;', '  -').strip()                                         # готовая строка с цитатой
+                link = hlink(quote_number, f'https://xn--80abh7bk0c.xn--p1ai{quote_link}')  # формируем ссылку для телеграм
+                quote = str(f'{link} - Добавлено {quote_date}\n{quote_text}')          # готовая цитата
+                quotes_array.append(quote)
 
         except UnicodeEncodeError:
-            print("Ошибка в получении цитат, еще попытка")
+            # print("Ошибка в получении цитат, еще попытка")
+            logging.info("Ошибка в получении цитат, еще попытка")
             load_quotes()
 
-        except sqlite3.OperationalError:
-            print("Ошибка записи в БД, проверь синтаксис")
-
-        finally:
-            connection.commit()
-            connection.close()
     else:
-        print('Что-то не так, переменная "article" пуста')
-
-
-create_database()
-load_quotes()
+        # print('Что-то не так, переменная "article" пуста')
+        logging.info('Что-то не так, переменная "article" пуста')
 
 
 def get_quote():
-    response = requests.get(url, verify=False)  # получаем страницу от сервера
-    soup = BeautifulSoup(response.text, 'lxml')  # создаем объект html страницы
-    articles = soup.find('section', class_='quotes')
-    article = articles.find('article', class_='quote')
-    quote_date = article.find('header', class_='quote__header').find('div',
-                                        class_='quote__header_date').text.strip()[0:10]  # получаем дату
-    quote_number = article.find('header', class_='quote__header').find('a',
-                                        class_='quote__header_permalink').text  # получаем номер цитаты
-    quote_link = article.find('header', class_='quote__header').find('a',
-                                        class_='quote__header_permalink').get('href')  # получаем ссылку на цитату
-    quote_text = str(article.find('div', class_='quote__body')) \
-        .replace('<br/>', '\n') \
-        .replace('</div>', '') \
-        .replace('<div class="quote__body">', '') \
-        .replace('&lt;', '  ') \
-        .replace('&gt;', '  -').strip()  # готовая строка с цитатой
-    link = hlink(quote_number, f'https://xn--80abh7bk0c.xn--p1ai{quote_link}')  # формируем ссылку для телеграмм
-    answer = str(f'{link} - Добавлено {quote_date}\n{quote_text}')  # формируем ответ для пользователя
+    """Достаем рандомную цитату, отправляем пользователю и удаляем из массива"""
+    if not quotes_array:
+        # print("Загружаю цитаты")
+        logging.info("Загружаю цитаты")
+        load_quotes()
+    quote_random_number = random.randint(0, len(quotes_array) - 1)
+    # print(f"В списке {len(quotes_array)} цитат, выбрана {quote_random_number}")
+    logging.info(f"В списке {len(quotes_array)} цитат, выбрана {quote_random_number}")
+    quote = quotes_array[quote_random_number]
+    quotes_array.pop(quote_random_number)
     timer_for_joke()
-    return answer
+    return quote
 
 
 @bot.message_handler(content_types=['text'])
@@ -189,7 +146,8 @@ def scanning_messages(message):
     chat_id = message.chat.id
     for i in keywords:
         if i in message.text.lower():
-            print(message.text)
+            # print(message.text)
+            logging.info(f"{message.from_user.first_name} что-то сказал(ла) про шутки? |{message.text}|")
             bot.send_message(message.chat.id, f"{message.from_user.first_name} что-то сказал(ла) про шутки?",
                              parse_mode='HTML')
             joke = get_quote()
@@ -198,6 +156,5 @@ def scanning_messages(message):
 
 
 print("Started...")
-# create_database()
-# bot.infinity_polling()
+bot.infinity_polling()
 
